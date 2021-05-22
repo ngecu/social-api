d797410f949c55b42915d1a05c548f7fe55dabc8
ngecu <devngecu@gmail.com>
2021-05-23 00:15:46 +0300

    before bycrypt

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const helmet = require("helmet");
+const mongoose  = require("mongoose");
+const morgan = require("morgan");
+const dotenv = require('dotenv');
+
+const userRoute = require('./routes/users')
+const authRoute = require('./routes/auth')
+
+
+const app = express();
+
+dotenv.config()
+
+//establish connection with
+mongoose.connect(process.env.MONGO_URL, 
+    {useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true})
+.then(()=> console.log("Connected"))
+.catch(err =>console.log(err))
+
+    
+
+
+
+//middlewares
+app.use(express.json())
+app.use(helmet())
+app.use(morgan("common"))
+
+app.use('/api/users',userRoute)
+app.use('/api/auth',authRoute)
+
+
+app.get('/users',(req,res)=>{
+    res.send("welcome to users")
+})
+
+//backend
+const PORT = 8800
+app.listen(PORT,()=>{
+    console.log("Backend server is ready")
+})
